499dc27bc9e58c8554c9bf1070c23d46c094a0f5
emng8 <emng888@gmail.com>
2024-09-27 13:41:15 -0600

    finished traffic light demo

--- traffic-light/sketch.js
+++ traffic-light/sketch.js
@@ -6,12 +6,11 @@
 // changing according to time. You may want to investigate the millis()
 // function at https://p5js.org/reference/#/p5/millis
 
-let isRed = false
-let isYellow = false
-let isGreen = false
-let lastSwitchtime = 0
-let redTime = 2000
-
+let lightState = "green";
+let lastTimeSwitched = 0;
+const GREEN_LIGHT_DURATION = 3000;
+const YELLOW_LIGHT_DURATION = 1000;
+const RED_LIGHT_DURATION = 4000;
 
 function setup() {
   createCanvas(600, 600);
@@ -20,6 +19,38 @@ function setup() {
 function draw() {
   background(255);
   drawOutlineOfLights();
+  switchStateIfNeeded();
+  displayCorrectLight();
+}
+
+function switchStateIfNeeded() {
+  if (lightState === "green" && millis() > lastTimeSwitched + GREEN_LIGHT_DURATION) {
+    lightState = "yellow";
+    lastTimeSwitched = millis();
+  }
+  else if (lightState === "yellow" && millis() > lastTimeSwitched + YELLOW_LIGHT_DURATION) {
+    lightState = "red";
+    lastTimeSwitched = millis();
+  }
+  else if (lightState === "red" && millis() > lastTimeSwitched + RED_LIGHT_DURATION) {
+    lightState = "green";
+    lastTimeSwitched = millis();
+  }
+}
+
+function displayCorrectLight() {
+  if (lightState === "green") {
+    fill("green");
+    ellipse(width/2, height/2 + 65, 50, 50); //bottom
+  }
+  else if (lightState === "yellow") {
+    fill("yellow");
+    ellipse(width/2, height/2, 50, 50); //middle
+  }
+  else if (lightState === "red") {
+    fill("red");
+    ellipse(width/2, height/2 - 65, 50, 50); //top
+  }
 }
 
 function drawOutlineOfLights() {
@@ -29,11 +60,8 @@ function drawOutlineOfLights() {
   rect(width/2, height/2, 75, 200, 10);
 
   //lights
-  if (millis() > lastSwitchtime + redTime)
-    isRed = !isRed
-  ellipse(width/2, height/2 - 65, 50, 50); //top 
-  isYellow = !isYellow
+  fill(255);
+  ellipse(width/2, height/2 - 65, 50, 50); //top
   ellipse(width/2, height/2, 50, 50); //middle
-  isGreen = !isGreen
   ellipse(width/2, height/2 + 65, 50, 50); //bottom
 }
