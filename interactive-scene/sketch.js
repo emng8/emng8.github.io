68fe1aac87a96739b4445428141b0861cbe5b465
emng8 <emng888@gmail.com>
2024-09-29 14:09:10 -0700

    cleaned up draw function and comments

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -19,11 +19,11 @@ let ballSpeed = 2.5;
 let ballDirectionX = -1;
 let ballDirectionY = -1;
 
-// player 1 
+// player 1 paddle
 let p1X = 10;
 let p1Y = 250;
 
-// player 2
+// player 2 paddle
 let p2X = 890;
 let p2Y = 250;
 
@@ -53,6 +53,11 @@ function setup() {
 } // close setup function
 
 function draw() {
+  gameStage();
+  displayWinner();
+} // close draw function
+
+function gameStage() {
   // show start screen
   if (stage === 0) {
     startScreen();  
@@ -67,17 +72,19 @@ function draw() {
   if(mouseIsPressed === true) {
     stage = 1; 
   }
-  
+} // close gameStage function
+
+function displayWinner() {
   // screen if player 1 wins
   if(stage === 2) {
     p1Wins();
   }
 
-    // screen if player 2 wins
+  // screen if player 2 wins
     if(stage === 3) {
       p2Wins();
     }
-} // close draw function
+} // close displayWinner function
 
 function startScreen() {
   background(169, 232, 182);
@@ -119,89 +126,87 @@ function p2Wins() {
   text('refresh to try again', width/2, 330);
 } // close p2Wins function
 
-
 function pong() {
-  // calling functions
-  keyTyped(); // looping keyTyped function
-  keyPressed(); // looping keyPressed function
+  // make wasd and arrow keys work
+  keyTyped();
+  keyPressed(); 
 
-  // draw court
-  background(240, 151, 170)
+  // drawing court
+  background(240, 151, 170);
   noFill();
-  stroke(255)
-  rect(width/2, height/2, width, height); // outer border
-  line(450, 0, 450, height) // center line
+  stroke(255);
+  rect(width/2, height/2, width, height); 
+  line(450, 0, 450, height); 
 
   // draw ball
   fill(0);
   noStroke();
-  rect(ballX, ballY, ballWidth, ballHeight)
+  rect(ballX, ballY, ballWidth, ballHeight);
 
   // draw player 1 and player 2 paddles
-  fill(131, 247, 135)
-  rect(p1X, p1Y, playerWidth, playerHeight)
-  rect(p2X, p2Y, playerWidth, playerHeight)
+  fill(131, 247, 135);
+  rect(p1X, p1Y, playerWidth, playerHeight);
+  rect(p2X, p2Y, playerWidth, playerHeight);
 
   // making ball move
-  ballX = ballX + (ballDirectionX * ballSpeed); // move horizontally
-  ballY = ballY + (ballDirectionY *ballSpeed); // move vertically
+  ballX = ballX + (ballDirectionX * ballSpeed); 
+  ballY = ballY + (ballDirectionY * ballSpeed); 
 
   // collision with walls
-  if(ballY >= height) { // hit bottom wall
-    ballDirectionY = ballDirectionY * (-1); // change direction
+  // if ball hits bottom wall, change direction
+  if (ballY >= height) { 
+    ballDirectionY = ballDirectionY * (-1); 
   }
-
-  if(ballY <= 0) { // hit top wall
-    ballDirectionY = ballDirectionY * (-1); // change direction
+  // if ball hits top wall, change direction
+  if (ballY <= 0) { 
+    ballDirectionY = ballDirectionY * (-1); 
   }
 
   // collision with paddles
-  if (ballX >= p1X - 10 && ballX <= p1X + 10 && ballY >= p1Y - 50 && ballY <= p1Y + 50) { // hit player 1
-    ballDirectionX = ballDirectionX * (-1); // change direction 
+  // if ball hits player 1 paddle, change direction
+  if (ballX >= p1X - 10 && ballX <= p1X + 10 && ballY >= p1Y - 50 && ballY <= p1Y + 50) {
+    ballDirectionX = ballDirectionX * (-1); 
+  } 
+  // if ball hits player 2 paddle, change direction
+  if (ballX >= p2X - 10 && ballX <= p2X + 10 && ballY >= p2Y - 50 && ballY <= p2Y + 50) { 
+    ballDirectionX = ballDirectionX * (-1); 
   } 
 
-  if (ballX >= p2X - 10 && ballX <= p2X + 10 && ballY >= p2Y - 50 && ballY <= p2Y + 50) { // hit player 2
-    ballDirectionX = ballDirectionX * (-1); // change direction 
-  } 
-
-  // scoreboard
+  // making scoreboard
   textSize(40);
   text(p1Score, 400, 50);
   text(p2Score, 500, 50);
 
-  // ball touches back wall, player 1 missed
+  // scoring points
+  // if player 1 misses, add point for player 2 and reset the ball
   if (ballX <= 0) { 
-    p2Score = p2Score + 1; // add point for player 2
-    // ball goes back to the center
+    p2Score = p2Score + 1; 
     ballX = width/2;
     ballY = height/2;
   }
-
-  // ball touches back wall, player 2 missed
+  // if player 2 misses, add point for player 1 and reset the ball
   if (ballX >= width) { 
-    p1Score = p1Score + 1; // add point for player 1
-    // ball goes back to the center
+    p1Score = p1Score + 1; 
     ballX = width/2;
     ballY = height/2;
   }
 
   // show screen if player 1 wins
-  if(p1Score >= 10) {
+  if (p1Score >= 10) {
     stage = 2;
   }
 
   // show screen if player 2 wins
-  if(p2Score >= 10) {
+  if (p2Score >= 10) {
     stage = 3;
   }
-
 } // close pong function
 
 function keyTyped(){
   if (key === 'w' && keyIsPressed) {
     p1Y = p1Y - pSpeed;
   }
-  if(key === 's' && keyIsPressed) {
+  if (key === 's' && keyIsPressed) {
     p1Y = p1Y + pSpeed;
   }
 } // close keyTyped function
@@ -211,7 +216,7 @@ function keyPressed() {
     p2Y = p2Y - pSpeed;
   }
 
-  if(keyCode === DOWN_ARROW && keyIsPressed) {
+  if (keyCode === DOWN_ARROW && keyIsPressed) {
     p2Y = p2Y + pSpeed;
   }
    } // close keyPressed Function
