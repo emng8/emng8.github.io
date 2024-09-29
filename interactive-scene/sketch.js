7216a65bc23603c5cadba5194a4a381b0650827e
emng8 <emng888@gmail.com>
2024-09-28 22:16:23 -0700

    fixed background for interactive scene

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -4,18 +4,32 @@
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
+// rectMode() changes the origin of the rectangle to be the center
+// 
 
+// global variables
+var ballX;
+var ballY;
+var ballWidth = 15;
+var ballHeight = 15;
 
 function setup() {
-  createCanvas(1000, 1000);
-}
+  createCanvas(900, 500);
+
+  // initial ball position
+  rectMode(CENTER);
+  ballX = width/2;
+  ballY = height/2;
+
+
+} // close setup function
 
 function draw() {
   background(218, 244, 245)
-  drawpaintpalette()
-}
 
-function drawpaintpalette() {
-  fill(217, 190, 169)
-  circle(250, 500, 300)
-}
+  // draw ball
+  fill(255);
+  nostroke();
+  rect(ballX, ballY, ballWidth, ballHeight)
+
+} // close draw function
