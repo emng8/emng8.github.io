3386abc368c1ddce17e2dda8aeaaadca8b0a5705
emng8 <emng888@gmail.com>
2024-10-08 14:13:48 -0600

    wrote project idea about making optical illusion or repetition art

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -5,6 +5,8 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+// Project Idea: make some sort of optical illusion or repetition art. probably the square optical illusion or trinangle pattern. 
+// might make it 3D so it shows it from all angles using 3D rendering. \
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
