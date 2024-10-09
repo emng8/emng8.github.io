f6c9d6b1b691803ec692942fd7e838c2019aa532
emng8 <emng888@gmail.com>
2024-10-09 14:04:59 -0600

    made small circles spinning in a big circle. learning about sin and cos in p5js

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -6,12 +6,25 @@
 // - describe what you did to take this project "above and beyond"
 
 // Project Idea: make some sort of optical illusion or repetition art. probably the square optical illusion or trinangle pattern. 
-// might make it 3D so it shows it from all angles using 3D rendering. \
+// might make it 3D so it shows it from all angles using 3D rendering. 
+
+// function setup() {
+//   createCanvas(windowWidth, windowHeight);
+// }
+
+// function draw() {
+//   background(220);
+// }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  translate(200, 200);
+
+  let x = cos(frameCount) * 100;
+  let y = sin(frameCount) * 100;
+
+  ellipse(x, y, 20, 20);
 }
