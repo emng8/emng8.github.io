080e06980f3d5961b8ceec3d0772148e4b5b01ee
emng8 <emng888@gmail.com>
2024-10-20 00:30:10 -0700

    trying to make the dots for the spiral

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -9,12 +9,26 @@
 // might make it 3D so it shows it from all angles using 3D rendering. 
 
 function setup() {
-  const size = min(window.innerWidth, window.innerHeight);
+  const size = min(windowWidth, windowHeight);
   createCanvas(size, size);
+  colorMode(RGB, 1);
   noStroke();
-  colorMode(HSL, 1);
 }
 
+const radius = 0.25;
+
 function draw() {
-  
+  scale(width, height);
+  background(0);
+  fill(1);
+
+  const count = 100;
+  for (let i = 0; i < count; i++) {
+    const f = i / count;
+    const a = i / PI;
+    const dist = f * radius;
+    const x = 0.5 + cos(a * TWO_PI) * dist;
+    const y = 0.5 + sin(a * TWO_PI) * dist;
+    circle(x, y, r);
+  }
 }
