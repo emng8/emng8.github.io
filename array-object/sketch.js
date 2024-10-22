49a934b70cd2cbac4af111182817160ec4c43d9b
emng8 <emng888@gmail.com>
2024-10-22 14:09:15 -0600

    testing github.

--- array-object/sketch.js
+++ array-object/sketch.js
@@ -3,18 +3,13 @@
 // October 21, 2024
 //
 // Extra for Experts:
-// 1. const size = min(windowWidth, windowHeight) - I learned that I can calculate the shortest
-// length of either the width or the height to ensure that the canvas is square.
-// 2. ColorMode(HSL, 1) - I learned that for my project, I can HSL to change colors based off of hue,
-// saturation, and lightness. Using HSL instead of RGB made the art more colorful. It also provides smooth 
-// color transitions. 
-// 3. cos() and sin() - I used it to draw the circles/spiral. The angle determines the angle of the rotation and the 
-// radius represents how far away it is from the origin of the vector. This makes up something called polar coordinates.
-// // 4. const - I used this to define a constant. After I declared it as a constant, it can not be reassigned. I had
-// to use this instead of let because if I used let, it would change everytime. 
+// 1. const size = min(windowWidth, windowHeight) - I learned that I can calculate the shortest length of either the width or the height to ensure that the canvas is square.
+// 2. ColorMode(HSL, 1) - I learned that for my project, I can HSL to change colors based off of hue, saturation, and lightness. Using HSL instead of RGB made the art more colorful. It also provides smooth color transitions. 
+// 3. cos() and sin() - I used it to draw the circles/spiral. The angle determines the angle of the rotation and the radius represents how far away it is from the origin of the vector. This makes up something called polar coordinates.
+// 4. const - I used this to define a constant. After I declared it as a constant, it can not be reassigned. I had to use this instead of let because if I used let, it would change everytime. 
 // 5. FrameCount() - I used this to track the number of frames drawn since the sketch started.I used this to create the animation with a loop.
-// 6. TWO_PI - I used this to convert angles from a normalized range (0 to 1) to radians. It essentially helps toz
-// 7. PHI - I used this to make the angle increase by the value of the golden ratio after each iteration. 
+// 6. TWO_PI - I used this to convert angles from a normalized range (0 to 1) to radians. It essentially helps to create the spiral pattern.
+// 7. PHI - I used this to make the angle increase by the value of the golden ratio after each iteration. This distributes the circles for a balanced look while making the spiral pattern. 
 
 // calculates the golden ratio
 const PHI = (1 + Math.sqrt(5)) / 2;
@@ -70,7 +65,7 @@ function createDots(t) {
     const angle = i * PHI; 
 
     // Determines the x, y, and distance from the center for the circle
-    const distance = fraction * shapeRadius; // calculates the distance from the center for each dot, scaling it by fraction to ensure dots spread out until the shapeRadius as i increases.
+    const distance = fraction * shapeRadius; // calculates the distance from the center for each dot so they spread out
     const x = 0.5 + cos(angle * TWO_PI) * distance;
     const y = 0.5 + sin(angle * TWO_PI) * distance; 
 
