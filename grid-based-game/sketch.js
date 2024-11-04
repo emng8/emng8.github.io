fccb8e8e75d9d9a077b9b992524a0b5b9446e078
emng8 <emng888@gmail.com>
2024-11-04 14:12:26 -0600

    added the other images I want to use for my game. I also started to set my grid up.

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -7,14 +7,20 @@
 // making a game where the bee has to go polinate all the flowers and then has to find its way back to the beehive
 
 let grid;
-const GRID_SIZE = 10;
+const GRID_SIZE = 50;
+const cellSize = 5;
 let grassIMG;
 let dirtIMG;
+let flowerIMG;
+let beeIMG; 
+let beehiveIMG;
 
 function preload() {
   grassIMG = loadImage("grass.png");
   dirtIMG = loadImage("dirt.png");
-  flowerIMG = loadImage("flowers.png");
+  flowerIMG = loadImage("flower.png");
+  beeIMG = loadImage("bee.png");
+  beehiveIMG = loadImage("beehive.png");
 }
 
 function setup() {
@@ -33,5 +39,9 @@ function draw() {
 }
 
 function displayGrid() {
-
+  for (let y = 0; y < GRID_SIZE; y++) {
+    for (let x = 0; x < GRID_SIZE; x++) {
+      square(x * cellSize, y * cellSize, cellSize);
+    }
+  }
 }
